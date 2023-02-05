fad945e3426b7e348899437c2be6a44a4386d1ae
tule1102 <tule1102@knights.ucf.edu>
2023-02-05 16:04:30 -0500

    converted jsx files to js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          {/* Edit <code>src/App.jsx</code> and save to test HMR */}
+          Does it work?
         </p>
       </div>
       <p className="read-the-docs">
