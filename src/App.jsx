94010100413a605847d64142b97df9608d5186f2
tule1102 <tule1102@knights.ucf.edu>
2023-02-05 16:34:03 -0500

    seeing if front end works

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [itemText, setItemText] = useState('');
+  const [listItems, setListItems] = useState([]);
+  const [isUpdating, setIsUpdating] = useState('');
+  const [updateItemText, setUpdateItemText] = useState('');
+
+    //add new todo item to database
+    const addItem = async (e) => {
+      e.preventDefault();
+      try{
+        const res = await axios.post('http://localhost:5500/api/item', {item: itemText})
+        setListItems(prev => [...prev, res.data]);
+        setItemText('');
+      }catch(err){
+        console.log(err);
+      }
+    }
+  
+    //Create function to fetch all todo items from database -- we will use useEffect hook
+    useEffect(()=>{
+      const getItemsList = async () => {
+        try{
+          const res = await axios.get('http://localhost:5500/api/items')
+          setListItems(res.data);
+          console.log('render')
+        }catch(err){
+          console.log(err);
+        }
+      }
+      getItemsList()
+    },[]);
+  
+    // Delete item when click on delete
+    const deleteItem = async (id) => {
+      try{
+        const res = await axios.delete(`http://localhost:5500/api/item/${id}`)
+        const newListItems = listItems.filter(item=> item._id !== id);
+        setListItems(newListItems);
+      }catch(err){
+        console.log(err);
+      }
+    }
+  
+    //Update item
+    const updateItem = async (e) => {
+      e.preventDefault()
+      try{
+        const res = await axios.put(`http://localhost:5500/api/item/${isUpdating}`, {item: updateItemText})
+        console.log(res.data)
+        const updatedItemIndex = listItems.findIndex(item => item._id === isUpdating);
+        const updatedItem = listItems[updatedItemIndex].item = updateItemText;
+        setUpdateItemText('');
+        setIsUpdating('');
+      }catch(err){
+        console.log(err);
+      }
+    }
+    //before updating item we need to show input field where we will create our updated item
+    const renderUpdateForm = () => (
+      <form className="update-form" onSubmit={(e)=>{updateItem(e)}} >
+        <input className="update-new-input" type="text" placeholder="New Item" onChange={e=>{setUpdateItemText(e.target.value)}} value={updateItemText} />
+        <button className="update-new-btn" type="submit">Update</button>
+      </form>
+    )
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Todo List</h1>
+      <form className="form" onSubmit={e => addItem(e)}>
+        <input type="text" placeholder='Add Todo Item' onChange={e => {setItemText(e.target.value)} } value={itemText} />
+        <button type="submit">Add</button>
+      </form>
+      <div className="todo-listItems">
+        {
+          listItems.map(item => (
+          <div className="todo-item">
+            {
+              isUpdating === item._id
+              ? renderUpdateForm()
+              : <>
+                  <p className="item-content">{item.item}</p>
+                  <button className="update-item" onClick={()=>{setIsUpdating(item._id)}}>Update</button>
+                  <button className="delete-item" onClick={()=>{deleteItem(item._id)}}>Delete</button>
+                </>
+            }
+          </div>
+          ))
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
