da519b0d6f1e685122bdce41b7c0cbee6991fdb5
tule1102 <tule1102@knights.ucf.edu>
2023-02-05 16:27:40 -0500

    trying again

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis", //<-- AWS SDK 
+      },
+    },
+  },
   resolve: {
     alias: {
       './runtimeConfig': './runtimeConfig.browser',
